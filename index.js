3d50d93ffc583d847fa122bd4a4739a77c315a1d
linychuo <linychuo@gmail.com>
2018-11-09 21:13:41 +0800

    refactor code and add eslint config

--- index.js
+++ index.js
@@ -1,36 +1,36 @@
-'use strict';
 const request = require('request');
 const bundles = require('./bundles.json');
 const fs = require('fs');
 const parser = require('./parser');
 
 const test = (name, _url) => {
-    request(_url, (error, response, body) => {
-        var { newVersion, downloadURL } = parser[bundles[name].parser](body);
-        if (newVersion !== bundles[name].version) {
-            console.log(newVersion);
-            if (!fs.existsSync(`./${name}-dist`)) {
-                fs.mkdirSync(`./${name}-dist`);
-            }
-            var ws = fs.createWriteStream(`./${name}-dist/${name}-${newVersion}.exe`);
-            ws.on('close', () => {
-                console.log(`${name} download finished...........`);
-                bundles[name].version = newVersion;
-                console.log(bundles);
-                fs.writeFileSync('bundles.json', JSON.stringify(bundles, null, 2));
-            })
-            request(downloadURL).pipe(ws);
-        }
-    });
+	request(_url, (error, response, body) => {
+		let { newVersion, downloadURL } = parser[bundles[name].parser](body);
+		if (newVersion !== bundles[name].version) {
+			console.log(`newVersion: ${newVersion}`);
+			if (!fs.existsSync(`./${name}-dist`)) {
+				fs.mkdirSync(`./${name}-dist`);
+			}
+			let ws = fs.createWriteStream(`./${name}-dist/${name}-${newVersion}.exe`);
+			ws.on('close', () => {
+				console.log(`${name} download finished...........`);
+				bundles[name].version = newVersion;
+				console.log(bundles);
+				fs.writeFileSync('bundles.json', JSON.stringify(bundles, null, 2));
+			});
+			console.log(`downloadURL: ${downloadURL}`);
+			request(downloadURL).pipe(ws);
+		}
+	});
 };
 
 const cmdArgv = process.argv;
 if (cmdArgv.length < 3) {
-    console.log('...............');
-} else {
-    var bunleName = cmdArgv[2];
-    if (bundles[bunleName]) {
-        test(bunleName, bundles[bunleName]);
-    }
+	console.log('...............');
 }
-
+else {
+	let bunleName = cmdArgv[2];
+	if (bundles[bunleName]) {
+		test(bunleName, bundles[bunleName]);
+	}
+}
--- parser.js
+++ parser.js
@@ -2,18 +2,14 @@ const $ = require('cheerio');
 const url = require('url');
 
 exports.fetchVersionOfVSCode = body => {
-    var t = $(body).find('div.body a').filter((i, item) => {
-        return $(item).text() === 'Windows';
-    });
-    var fileURL = $(t[0]).prop('href');
-    console.log(fileURL);
-    var { pathname, hostname } = url.parse(fileURL);
-    return { 'newVersion': pathname.split('/')[1], 'downloadURL': fileURL };
+	let t = $(body).find('div.body a').filter((i, item) => $(item).text() === 'Windows');
+	let downloadURL = $(t[0]).prop('href');
+	let newVersion = url.parse(downloadURL).pathname.split('/')[1];
+	return { newVersion, downloadURL };
 };
 
 exports.fetchVersionOfSublime = body => {
-    var fileURL = $(body).find('span.win, span.show_64').find('a').prop('href');
-    console.log(fileURL);
-    var newVersion = $(body).find('div.current_version i').text();
-    return { 'newVersion': newVersion, 'downloadURL': fileURL };
+	let downloadURL = $(body).find('span.win, span.show_64').find('a').prop('href');
+	let newVersion = $(body).find('div.current_version i').text();
+	return { newVersion, downloadURL };
 };

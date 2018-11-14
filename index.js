d3f174f30ca214fcd74e3f54959b6c5c574b7bea
linychuo <linychuo@gmail.com>
2018-11-14 23:14:58 +0800

    refactor code, using 'async' instead of promise

--- index.js
+++ index.js
@@ -1,27 +1,42 @@
-const request = require('request');
+const request = require('request-promise-native');
 const bundles = require('./bundles.json');
 const fs = require('fs');
 const parser = require('./parser');
 
-const test = (name, _url) => {
-	request(_url, (error, response, body) => {
-		let { newVersion, downloadURL } = parser[bundles[name].parser](body);
-		if (newVersion !== bundles[name].version) {
-			console.log(`newVersion: ${newVersion}`);
-			if (!fs.existsSync(`./${name}-dist`)) {
-				fs.mkdirSync(`./${name}-dist`);
-			}
-			let ws = fs.createWriteStream(`./${name}-dist/${name}-${newVersion}.exe`);
-			ws.on('close', () => {
-				console.log(`[${name}] download finished...........`);
-				bundles[name].version = newVersion;
-				console.log(bundles);
-				fs.writeFileSync('bundles.json', JSON.stringify(bundles, null, 2));
-			});
-			console.log(`downloadURL: ${downloadURL}`);
-			request(downloadURL).pipe(ws);
-		}
+const getInfo = async (name, url) => {
+	let body = await request(url);
+	return parser[bundles[name].parser](body);
+};
+
+const downloadFile = (name, newVersion, downloadURL) => {
+	if (!fs.existsSync(`./${name}-dist`)) {
+		fs.mkdirSync(`./${name}-dist`);
+	}
+	let ws = fs.createWriteStream(`./${name}-dist/${name}-${newVersion}.exe`);
+	let closePromise = new Promise((resolve, reject) => {
+		ws.on('close', () => {
+			console.log(`[${name}] download finished...........`);
+			resolve();
+		});
 	});
+
+	console.log(`downloadURL: ${downloadURL}`);
+	request(downloadURL).pipe(ws);
+	return closePromise;
+};
+
+const updateInfo = (name, newVersion) => {
+	bundles[name].version = newVersion;
+	console.log(bundles);
+	fs.writeFileSync('bundles.json', JSON.stringify(bundles, null, 2));
+};
+
+const main = async (name, url) => {
+	let { newVersion, downloadURL } = await getInfo(name, url);
+	console.log(newVersion, downloadURL);
+	if (newVersion !== bundles[name].version) {
+		downloadFile(name, newVersion, downloadURL).then(() => updateInfo(name, newVersion));
+	}
 };
 
 const cmdArgv = process.argv;
@@ -29,13 +44,13 @@ if (cmdArgv.length < 3) {
 	console.log('.......All tasks will running async!!........');
 	Object.entries(bundles).forEach(([k, v]) => {
 		process.nextTick(() => {
-			test(k, v.url);
+			main(k, v.url);
 		});
 	});
 }
 else {
 	let bundleName = cmdArgv[2];
 	if (bundles[bundleName]) {
-		test(bundleName, bundles[bundleName].url);
+		main(bundleName, bundles[bundleName].url);
 	}
 }

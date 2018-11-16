5eb47285b6a3612f31b9b249f5db12ab00b290a3
linychuo <linychuo@gmail.com>
2018-11-16 23:20:32 +0800

    modify loggin content

--- index.js
+++ index.js
@@ -15,25 +15,25 @@ const downloadFile = (name, newVersion, downloadURL) => {
 	let ws = fs.createWriteStream(`./${name}-dist/${name}-${newVersion}.exe`);
 	let closePromise = new Promise((resolve, reject) => {
 		ws.on('close', () => {
-			console.log(`[${name}] download finished...........`);
+			console.log(`3. ${name} finish downloading file`);
 			resolve();
 		});
 	});
 
-	console.log(`downloadURL: ${downloadURL}`);
+	console.log(`2. ${name} start to download, url is ${downloadURL}`);
 	request(downloadURL).pipe(ws);
 	return closePromise;
 };
 
 const updateInfo = (name, newVersion) => {
 	bundles[name].version = newVersion;
-	console.log(bundles);
+	console.log(`4. update bundle[${name}] file`);
 	fs.writeFileSync('bundles.json', JSON.stringify(bundles, null, 2));
 };
 
 const main = async (name, url) => {
 	let { newVersion, downloadURL } = await getInfo(name, url);
-	console.log(newVersion, downloadURL);
+	console.log(`1. ${name}: current version is ${newVersion}, older Version is ${bundles[name].version}`);
 	if (newVersion !== bundles[name].version) {
 		downloadFile(name, newVersion, downloadURL).then(() => updateInfo(name, newVersion));
 	}
@@ -41,7 +41,7 @@ const main = async (name, url) => {
 
 const cmdArgv = process.argv;
 if (cmdArgv.length < 3) {
-	console.log('.......All tasks will running async!!........');
+	console.log('All tasks will running async!!');
 	Object.entries(bundles).forEach(([k, v]) => {
 		process.nextTick(() => {
 			main(k, v.url);

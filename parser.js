0d1413bb0e8700e0fabb13466a0e1e72983e13ec
linychuo <linychuo@gmail.com>
2018-12-05 21:51:02 +0800

    add downloading method of rust

--- parser.js
+++ parser.js
@@ -2,7 +2,7 @@ const $ = require('cheerio');
 const url = require('url');
 
 exports.fetchVersionOfVSCode = body => {
-	let t = $(body).find('div.body a').filter((i, item) => $(item).text() === 'Windows');
+	let t = $(body).find('div.body a').filter((i, el) => $(el).text() === 'Windows');
 	let downloadURL = $(t[0]).prop('href');
 	let newVersion = url.parse(downloadURL).pathname.split('/')[1];
 	return { newVersion, downloadURL };
@@ -11,5 +11,14 @@ exports.fetchVersionOfVSCode = body => {
 exports.fetchVersionOfSublime = body => {
 	let downloadURL = $(body).find('span.win, span.show_64').find('a').prop('href');
 	let newVersion = $(body).find('div.current_version i').text();
+	return { newVersion, downloadURL };
+};
+
+exports.fetchVersionOfRust = body => {
+	let t = $(body).find('div.installer-table.stable a')
+		.filter((i, el) => $(el).prop('href').endsWith('x86_64-pc-windows-gnu.msi'));
+	let downloadURL = $(t[0]).prop('href');
+	t = $(body).find('h3').filter((i, el) => $(el).text().startsWith('Stable'));
+	let newVersion = $(t[0]).text();
 	return { newVersion, downloadURL };
 };
